a83a51f74c5a3295ee5ebfd9b53d787773cf548d
Maciej Dąbrowski <maciej.dabrowski299@gmail.com>
2023-08-27 21:44:56 +0200

    add script to page

--- script.js
+++ script.js
@@ -13,8 +13,11 @@ const closeModalBtn = document.querySelector('.close')
 let minutes = 0
 let seconds = 0
 let countTime
+let timeArr = []
 
 const startTime = () => {
+	clearInterval(countTime)
+
 	countTime = setInterval(() => {
 		if (seconds < 9) {
 			seconds++
@@ -30,4 +33,58 @@ const startTime = () => {
 	}, 1000)
 }
 
+const pauseTime = () => {
+	clearInterval(countTime)
+}
+
+const stopTime = () => {
+	timeWatch.textContent = `Ostatni czas: ${timeStop.textContent}`
+	if (timeStop.textContent !== '0:00') {
+		timeWatch.style.visibility = 'visible'
+		timeArr.push(`${timeStop.textContent}`)
+	}
+	clearStuff()
+}
+
+const resetTime = () => {
+	timeWatch.style.visibility = 'hidden'
+	timeArr = []
+	clearStuff()
+}
+
+const clearStuff = () => {
+	timeStop.textContent = '0:00'
+	timeList.textContent = ''
+	minutes = 0
+	seconds = 0
+	clearInterval(countTime)
+}
+
+const showHistory = () => {
+	timeList.textContent = ''
+	let num = 1
+	timeArr.forEach(time => {
+		const newTime = document.createElement('li')
+		newTime.innerHTML = `Pomiar ${num} <span>${time}</span>`
+		timeList.appendChild(newTime)
+		num++
+	})
+}
+
+const showModal = () => {
+	if (!(modalShadow.style.display === 'block')) {
+		modalShadow.style.display = 'block'
+	} else {
+		modalShadow.style.display = 'none'
+	}
+	modalShadow.classList.toggle('modal-animation')
+}
+
 buttonStart.addEventListener('click', startTime)
+buttonPause.addEventListener('click', pauseTime)
+buttonStop.addEventListener('click', stopTime)
+buttonReset.addEventListener('click', resetTime)
+buttonHistory.addEventListener('click', showHistory)
+buttonInfo.addEventListener('click', showModal)
+closeModalBtn.addEventListener('click', showModal)
+window.addEventListener('click', e => (e.target === modalShadow ? showModal() : false))
